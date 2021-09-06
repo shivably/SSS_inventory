6f63fca90245eba7e54040f4aabe791141387c40
Senthil <senthilprakash2020@gmail.com>
2021-09-06 17:12:40 +0530

    Add Ui and save functionality

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,4 +4,5 @@ from inventory import views
 urlpatterns = [
     path("", views.product, name="product_list"),
     path("<int:product_id>/", views.detail, name="product_detail"),
+    path("add", views.add, name="product_add"),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -18,3 +18,25 @@ def detail(request, product_id):
     except Product.DoesNotExist:
         raise Http404("Question does not exist")
     return render(request, 'product/details.html', {'product': product})
+
+def add(request):
+
+    if request.method == 'GET':
+        template = loader.get_template('product/add.html')
+        context = {}
+        return HttpResponse(template.render(context, request))
+    if request.method == 'POST':
+        name = request.POST.get('name', None)
+        dsc = request.POST.get('description', None)
+        mft = request.POST.get('manufacturer', None)
+        price = request.POST.get('price_in_dollars', None)
+
+        new_product = Product(name=name, description=dsc, manufacturer=mft, price_in_dollars=price)
+        new_product.save()
+
+        product_list = Product.objects.order_by('price_in_dollars')
+        template = loader.get_template('product/list.html')
+        context = {
+            'product_list': product_list,
+        }
+        return HttpResponse(template.render(context, request))

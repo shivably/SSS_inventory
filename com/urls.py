06ccc1bed59bb27a1740e543700404472b7cf7e2
Shiva <shivably@gmail.com>
2021-09-25 11:57:09 +0530

    quick links and reports page fix

--- com/urls.py
+++ com/urls.py
@@ -7,6 +7,8 @@ from com.views import LogoutView
 from com.views import (
     CreateCustomer, CustomerTemplateView, CustomerUpdateView, CreateFeedBack)
 from com.views import RegisterView, ReportsView
+from com.api_views import DailySalesAPI, WeeklySalesAPI, MonthlySalesAPI
+
 
 urlpatterns = [
     re_path(r'^$', HomePageView.as_view(), name='index'),
@@ -14,7 +16,6 @@ urlpatterns = [
     re_path(r'^login/$', LoginView.as_view(), name='login'),
     re_path(r'^logout/$', LogoutView.as_view(), name='logout'),
 
-    #path('api/', include('com.api_urls', namespace='com_api')),
     # re_path(r'^api/', CreateCustomer.as_view(), name='create_customer'),
 
     re_path(r'^customer/create/$',
@@ -25,4 +26,10 @@ urlpatterns = [
     re_path(r'^customer/(?P<pk>\d+)/update$',
             CustomerUpdateView.as_view(), name='update_customer'),
     re_path(r'^register/$', RegisterView.as_view(), name='register'),
+    re_path(r'^api/sales/daily/$', DailySalesAPI.as_view(),
+            name='daily_sales_api'),
+    re_path(r'^api/sales/weekly/$', WeeklySalesAPI.as_view(),
+            name='weekly_sales_api'),
+    re_path(r'^api/sales/monthly/$', MonthlySalesAPI.as_view(),
+            name='monthly_sales_api'),
 ]
